33c19cdac011b118f359925609a85618fd1cd9cc
jppd <73227496+dantasjoao@users.noreply.github.com>
2024-06-27 12:04:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// In your controller file
+import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  async findAll() {
+    return this.prisma.post.findMany({
+      where: { published: true },
+      include: { author: true },
+    });
+  }
+}
